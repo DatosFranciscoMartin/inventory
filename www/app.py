e8a66773e8b210f57ec58e89bfd8d6dde87f2f59
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-11-08 13:37:09 +0100

    Agregados nuevas consultas de prueba

--- www/app.py
+++ www/app.py
@@ -22,12 +22,22 @@ def get_db_connection():
 # Ejemplo de ruta para obtener datos
 @app.route('/obtener_datos', methods=['GET'])
 def obtener_datos():
+    tabla = request.args.get('tabla')  # Obtener la tabla seleccionada del formulario
+    
+    if not tabla:
+        return "No se ha seleccionado ninguna tabla", 400
+    
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM Sitios")
+    
+    # Ejecutar consulta SQL dinámica
+    query = f"SELECT * FROM {tabla}"
+    cursor.execute(query)
     results = cursor.fetchall()
     cursor.close()
     conn.close()
+    
+    # Retornar los resultados en formato JSON
     return jsonify(results)
 
 
