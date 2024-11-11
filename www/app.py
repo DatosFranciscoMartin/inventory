45bebb6207ad9692ab3078b28294a96c4d651bb8
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-11-11 14:05:00 +0100

    Agregados ficheros de prueba

--- www/app.py
+++ www/app.py
@@ -1,45 +1,10 @@
-from flask import Flask, render_template, request, jsonify
-import mysql.connector
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Configuración de la conexión a MySQL
-def get_db_connection():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="myadmin",
-        password="password",
-        database="InventarioEquipos"
-    )
-    return connection
-
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
-# Ejemplo de ruta para obtener datos
-@app.route('/obtener_datos', methods=['GET'])
-def obtener_datos():
-    tabla = request.args.get('tabla')  # Obtener la tabla seleccionada del formulario
-    
-    if not tabla:
-        return "No se ha seleccionado ninguna tabla", 400
-    
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    
-    # Ejecutar consulta SQL dinámica
-    query = f"SELECT * FROM {tabla}"
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    
-    # Retornar los resultados en formato JSON
-    return jsonify(results)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
-
