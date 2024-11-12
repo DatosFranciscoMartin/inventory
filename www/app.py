396bc1654bb9d7bc6f956183cc559f745d6ca576
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-11-12 18:32:11 +0100

    Agregado menu desplegable

--- www/app.py
+++ www/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# Rutas adicionales
+@app.route('/pagina1')
+def pagina1():
+    return render_template('pagina1.html')
+
+@app.route('/pagina2')
+def pagina2():
+    return render_template('pagina2.html')
+
+@app.route('/pagina3')
+def pagina3():
+    return render_template('pagina3.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
