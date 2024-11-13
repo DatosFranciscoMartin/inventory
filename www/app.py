692e65a816fb94b445b63f5ea81989c9acc2351c
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-11-13 13:13:01 +0100

    Agregadas consultas

--- www/app.py
+++ www/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, request
 from config import Config
 from modelos import db, Cliente, Contacto, Sitio, Equipo  # Importa los modelos necesarios
 
@@ -14,7 +14,7 @@ def home():
     return render_template('index.html')
 
 # Rutas adicionales
-@app.route('/pagina1')
+@app.route('/pagina_clientes')
 def pagina1():
     return render_template('pagina1.html')
 
@@ -29,6 +29,45 @@ def pagina3():
 @app.route('/pagina_base')
 def pagina_base():
     return render_template('pagina_base.html')
+
+@app.route('/consultas', methods=['GET'])
+def consultas():
+    return render_template('consultas.html')
+
+@app.route('/consultar', methods=['POST'])
+def consultar():
+    consulta = request.form.get('consulta')
+    resultados = []
+
+    if consulta == 'clientes':
+        resultados = [{'id': cliente.id, 'nombre': cliente.nombre} for cliente in Cliente.query.all()]
+    elif consulta == 'sitios':
+        resultados = [{'id': sitio.id, 'nombre': sitio.nombre, 'ubicacion': sitio.ubicacion} for sitio in Sitio.query.all()]
+    elif consulta == 'equipos':
+        resultados = [
+            {
+                'id': equipo.id,
+                'proveedor': equipo.proveedor,
+                'marca': equipo.marca,
+                'nserie': equipo.nserie,
+                'fecha_compra': equipo.fecha_compra,
+                'esta_en_soporte': equipo.esta_en_soporte
+            }
+            for equipo in Equipo.query.all()
+        ]
+    elif consulta == 'contactos':
+        resultados = [
+            {
+                'id': contacto.id,
+                'nombre': contacto.nombre,
+                'apellidos': contacto.apellidos,
+                'correo_electronico': contacto.correo_electronico,
+                'telefono': contacto.telefono
+            }
+            for contacto in Contacto.query.all()
+        ]
+
+    return render_template('consultas.html', resultados=resultados)
 
 if __name__ == '__main__':
     app.run(debug=True)
