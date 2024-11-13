200df19a50c5fa41e2d935ec39e3951484df5e82
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-11-13 13:55:56 +0100

    Cambio de usuario de mysql

--- www/config.py
+++ www/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Datos!2022@localhost/InventarioEquipos'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://flask_user:Datos!2022@localhost/InventarioEquipos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
