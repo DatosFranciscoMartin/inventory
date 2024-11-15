a4075dd92bccaf9e9f6e1e606076a1031cba189a
DatosFranciscoMartin <franciscojavier.martin@datosmedia.es>
2024-11-15 10:56:18 +0100

    Agregada clave de respaldo

--- www/config.py
+++ www/config.py
@@ -1,3 +1,6 @@
+import os
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://flask_user:Datos!2022@localhost/InventarioEquipos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'd2e7f842f5b86e3f4f83d8e7362392e65028c59c87f87e1f46a3b6183d9e2a8d'
